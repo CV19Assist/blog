1569bf3201a5369e04b0421abeaf2bcc9b1513ce
Farhan Ahmad <farhan@thebitguru.com>
2020-04-04 23:16:39 -0500

    Created the simple generation script.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# blog
-Content for our blog.
+# CV19 Assist Blog
+
+The content for our blog is written in Markdown, manually converted to HTML and then retrieved on the single page app.  This is just the starting point.
+
+To see the blog please visit https://cv19assist.com/blog
+
+
+### Conventions
+
+The blog files are stored in the `entries` folder.  Each entry should start with the ISO 8601 timestamp followed by two hyphens (`--`) and then a user friendly title.  The title is only there for easily identifying the entries and will be ignored.  The timestamp can be seen at [UTC Time ](https://www.utctime.net).
+
+
+## Build
+
+The build is done using github Actions and result in `.json` files in the `docs` folder which are then exposed using github pages.
---
+++ index.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const fs = require("fs");
+const showdown = require("showdown");
+const converter = new showdown.Converter({ metadata: true });
+
+const dirPath = path.join(__dirname, "entries");
+const outputPath = path.join(__dirname, "docs");
+
+let entryIndex = [];
+let entries = {};
+
+fs.readdir(dirPath, (err, filenames) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  // console.log(filenames);
+  filenames.forEach((filename) => {
+    let data = fs.readFileSync(path.join(dirPath, filename), "utf8");
+
+    if (!filename.match(/^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z)(--.*)?.md$/)) {
+      console.log(`Unexpected filename '${filename}'`);
+      return;
+    }
+
+    // console.log(filename);
+    // console.log(data);
+
+    const html = converter.makeHtml(data);
+    let indexEntry = converter.getMetadata();
+    let id = path.basename(filename, ".md");
+    indexEntry.id = id;
+    indexEntry.createdAt = id;
+    if (!indexEntry.slug) {
+      console.log(`Metadata 'slug' is required, not found in "${filename}". Ignoring this file.`);
+      return;
+    }
+    if (!indexEntry.slug.match(/^\S{2,}$/)) {
+      console.log(`Slug must be at least two non-whitespace characters, found '${indexEntry.slug}' in "${filename}". Ignoring this file.`);
+      return;
+    }
+
+    // Validate that the slug is unique
+    if (entries[indexEntry.slug]) {
+      console.log(`Already found slug "${indexEntry.slug}", slug must be unique in "${filename}". Ignoring this file.`);
+      return;
+    }
+
+    entryIndex.push(indexEntry);
+
+    let bodyEntry = { ...indexEntry };
+    bodyEntry.html = html;
+    entries[indexEntry.slug] = bodyEntry;
+
+    // console.log(indexEntry);
+    // console.log(html);
+    // });
+  });
+  // console.log(entryIndex);
+  // console.log(entries);
+
+  // Finally write the output files.
+  fs.writeFileSync(path.join(outputPath, "index.json"), JSON.stringify(entryIndex), "utf8");
+  Object.keys(entries).forEach(entrySlug => {
+    fs.writeFileSync(path.join(outputPath, `entry-${entrySlug}.json`), JSON.stringify(entries[entrySlug]), "utf8");
+  });
+  console.log(`Generated ${entryIndex.length} entries.`)
+});
